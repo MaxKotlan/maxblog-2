866f5d67056c981c77a0898ba7cb8dcd057fe681
Greg DeLozier <gdelozie@kent.edu>
2019-10-21 22:06:49 -0400

    Updates for error handling

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 export FLASK_APP=microblog.py
-export FLASK_DEBUG=1
+export FLASK_DEBUG=0
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME="prof.cortana@gmail.com"
+export MAIL_PASSWORD="example.44281"
 flask run
+
